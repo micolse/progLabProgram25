beba607a73d4053e5a289b585d60dceece5b2dbb
micolse <micol.serri@edu.unifi.it>
2025-07-16 14:32:02 +0200

    ignoro cartella build

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_progLabProgram25_FILE C:/Users/micol/OneDrive/Documents/progLabProgram25/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/progLabProgram25.exe)
-set(__QT_DEPLOY_TARGET_progLabProgram25_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_progLabProgram25_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
